54398f09e9cbb5f5cb4a20f268b5dc2b86f75b8b
belaab <izabela150b@gmail.com>
2018-06-21 11:39:24 +0200

    cleanup

--- src/main/java/pl/edu/agh/JsonTest.java
+++ src/main/java/pl/edu/agh/JsonTest.java
@@ -3,10 +3,10 @@ package pl.edu.agh;
 public class JsonTest {
     public static void main(String[] args) {
         Json json = new Json();
-        json.addValue("jebando", "wdupando");
-        json.addValue("niejebndo", "niewdupando");
+        json.addValue("klucz1", "wartosc1");
+        json.addValue("klucz2", "wartosc2");
         System.out.println(
-                "getValue(\"jebando\"): " + json.getValue("jebando")
+                "getValue(\"wartosc2\"): " + json.getValue("wartosc1")
         );
         System.out.println(
                 "\n\ngetJson():\n" + json.getJson()
